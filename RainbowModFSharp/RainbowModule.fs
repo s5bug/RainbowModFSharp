978e2fb413e166e23694792912f8954df878ad26
Anthony Cerruti <soren@disroot.org>
2020-08-18 23:39:39 -0700

    Initial commit

---
+++ RainbowModFSharp/RainbowModule.fs
@@ -0,0 +1,56 @@
+﻿namespace Celeste.Mod.RainbowFs
+
+open Celeste
+open Celeste.Mod
+open FMOD.Studio
+open Microsoft.Xna.Framework
+open Monocle
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Reflection
+open System.Text
+open System.Threading.Tasks
+open Mono.Cecil.Cil
+
+type RainbowModule() =
+    inherit EverestModule()
+    
+    static let mutable instance: Option<RainbowModule> = None
+    static member Instance with get(): Option<RainbowModule> = instance
+    
+    override this.Load() =
+        match RainbowModule.Instance with
+        | Some(x) -> Logger.Log(LogLevel.Warn, "rainbowmodfs", "Load called while instance already loaded!")
+        | None -> instance <- Some this
+        
+        On.Celeste.PlayerHair.add_GetHairColor(On.Celeste.PlayerHair.hook_GetHairColor(RainbowModule.GetHairColor))
+        
+    override this.Unload() =
+        match RainbowModule.Instance with
+        | Some(x) -> instance <- None
+        | None -> Logger.Log(LogLevel.Warn, "rainbowmodfs", "Unload called while no instance loaded!")
+
+    override this.SettingsType: Type = typeof<RainbowModuleSettings>
+    static member Settings with get(): Option<RainbowModuleSettings> = Option.map (fun (i: RainbowModule) -> i._Settings :?> _) RainbowModule.Instance
+    
+    override this.LoadSettings() =
+        base.LoadSettings()
+        match RainbowModule.Settings with
+        | Some(settings) ->
+            let updateLight = settings.FoxColorLight.A = byte 0
+            let updateDark = settings.FoxColorDark.A = byte 0
+            let update = updateLight || updateDark
+            
+            if updateLight then settings.FoxColorLight <- Color(0.8f, 0.5f, 0.05f, 1.0f)
+            if updateDark then settings.FoxColorDark <- Color(0.1f, 0.05f, 0.0f, 1.0f)
+            
+            if update then this.SaveSettings()
+        | None -> Logger.Log(LogLevel.Warn, "rainbowmodfs", "LoadSettings called but no Settings were available!")
+
+    static member GetHairColor (orig: On.Celeste.PlayerHair.orig_GetHairColor) (self: PlayerHair) (index: int): Color =
+        let colorOrig = orig.Invoke(self, index)
+        if not (self.Entity :? Player) || self.GetSprite().Mode = PlayerSpriteMode.Badeline
+            then colorOrig
+            else
+                invalidOp "Unimplemented"
---
+++ RainbowModFSharp/RainbowModuleSettings.fs
@@ -0,0 +1,68 @@
+﻿namespace Celeste.Mod.RainbowFs
+
+open Celeste.Mod
+open FMOD.Studio
+open Microsoft.Xna.Framework
+open Monocle
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Linq
+open System.Text
+open System.Threading.Tasks
+open YamlDotNet.Serialization
+
+type RainbowModuleSettings() =
+    inherit EverestModuleSettings()
+    
+    member val RainbowEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    [<SettingRange(0, 20)>]
+    member val RainbowSpeed: int = 10 with get, set
+    [<YamlIgnore>]
+    [<SettingIgnore>]
+    member this.RainbowSpeedFactor = float32 this.RainbowSpeed / 20.0f
+    
+    member val FoxEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    [<YamlMember(Alias = "FoxColorLight")>]
+    [<SettingIgnore>]
+    member this.FoxColorLightHex
+        with get() = this.FoxColorLight.R.ToString("X2") + this.FoxColorLight.G.ToString("X2") + this.FoxColorLight.B.ToString("X2")
+        and set(value: string) =
+            if String.IsNullOrEmpty(value) then () else
+                try this.FoxColorLight <- Calc.HexToColor(value)
+                with
+                    (e: Exception) ->
+                        Logger.Log(LogLevel.Warn, "rainbowmodfs", "Invalid FoxColorLight!")
+                        Logger.LogDetailed(e)
+
+    [<YamlIgnore>]
+    [<SettingIgnore>]
+    member val FoxColorLight: Color = Unchecked.defaultof<Color> with get, set
+    
+    [<YamlMember(Alias = "FoxColorDark")>]
+    [<SettingIgnore>]
+    member this.FoxColorDarkHex
+        with get() = this.FoxColorDark.R.ToString("X2") + this.FoxColorDark.G.ToString("X2") + this.FoxColorDark.B.ToString("X2")
+        and set(value: string) =
+            if String.IsNullOrEmpty(value) then () else
+                try this.FoxColorDark <- Calc.HexToColor(value)
+                with
+                    (e: Exception) ->
+                        Logger.Log(LogLevel.Warn, "rainbowmodfs", "Invalid FoxColorLight!")
+                        Logger.LogDetailed(e)
+
+    [<YamlIgnore>]
+    [<SettingIgnore>]
+    member val FoxColorDark: Color = Unchecked.defaultof<Color> with get, set
+
+    member val WoomyEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    member val SkateboardEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    member val DuckToDabEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    member val DuckToSneezeEnabled: bool = Unchecked.defaultof<bool> with get, set
+    
+    member val BaldelineEnabled: bool = Unchecked.defaultof<bool> with get, set
